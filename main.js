41e867476de23df9a9db1758f5efa8786a52081e
Caden Preece <caden.preece@gmail.com>
2021-02-28 01:43:29 -0700

    Add Files

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Import required libraries 
+const express = require("express"),
+app = express(), 
+layouts = require("express-ejs-layouts");
+
+// Import Mongoose to work with MongoDB 
+const mongoose = require("mongoose");
+
